37e4810ea5951e1cfd8c20a2ce9364e5f286e7fe
Stuart Swartz <stuart.swartz@parivedasolutions.com>
2017-12-22 14:38:33 -0600

    testing angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
